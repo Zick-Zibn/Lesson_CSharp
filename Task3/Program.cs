a549579aec3563f4b5fd8483e3835bb3c3e80ec0
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-12 02:46:30 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,11 @@
-﻿Co
+﻿int number;
+
+Console.WriteLine("Определение четного числа");
+Console.Write("Введите число:");
+
+number = int.Parse(Console.ReadLine() ?? "");
+
+if (number % 2 == 0)
+    Console.WriteLine("Введенное число является четным");
+else
+    Console.WriteLine("Введенное число является не четным");
