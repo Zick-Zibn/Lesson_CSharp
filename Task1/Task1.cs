f112685bded7266a45c221e986022fa366cc7bc7
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-12 01:17:24 +0300

    Add code

--- Task1/Task1.cs
+++ Task1/Task1.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1;
+int number2;
+
+Console.Clear();
+Console.WriteLine("Поиск наибольшего числа из двух");
+
+Console.Write("Введите первое число:");
+number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:");
+number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} больше числа {number1}");   
+}
