15a9b91a8fb290cb86df20b6d43e4dac04493fcb
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-12 02:59:32 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿int number;
+int i = 1;
+
+Console.WriteLine("Вывод четных чисел");
+Console.Write("Введите произвольное число:");
+
+number = int.Parse(Console.ReadLine() ?? "");
+
+while (i <= number)
+{
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+    i++;
+}
