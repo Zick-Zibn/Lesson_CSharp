3c60acd7eebc7ae68782f8900ca169d7ba14c5a4
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-14 11:01:56 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿int     number;
+int     secondNumber;
+string  value;
+bool    canParse;
+
+while (true)
+{
+    Console.Write("Введите трехзначное число");
+    value = Console.ReadLine() ?? "";
+
+    canParse = int.TryParse(value, out number);
+
+    if (canParse && (number > 99 && number < 1000))
+    {
+        secondNumber = (number / 10) % 10;
+        Console.WriteLine($"Вторая цифра числа {number} равна {secondNumber}");
+        break;
+    }
+    else
+        Console.WriteLine("Введенное число не трехзначное");
+}
+
+
