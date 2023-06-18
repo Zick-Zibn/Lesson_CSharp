31444b232bb893c9c4b97bef87b4be97a41d496e
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-19 00:39:13 +0300

    Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int     number;
+int     i = 1;
+string  value;
+bool    canParse;
+
+while (true)
+{
+    Console.WriteLine("Введите число: ");
+    value = Console.ReadLine()?? "";
+    canParse = int.TryParse(value, out number);
+
+    if (canParse)
+    {
+        Console.Write($"{number} -> ");
+
+        while (i <= number)
+        {
+            Console.Write("{0}{1}", numberCube(i), i != number ? ", " : "");
+            i++;
+        }
+        break;
+    }
+    else
+        Console.WriteLine("Введеное значение не является числом");
+}
+
+static int numberCube(int i)
+{
+    return Convert.ToInt32(Math.Pow(i, 3));
+}
