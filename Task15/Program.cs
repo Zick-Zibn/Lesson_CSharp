d1bbc2389b523b2305b2cfb175011fa1e0b72ae7
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-18 20:58:49 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,22 @@
+﻿int     dayWeek;
+string  value;
+bool    canParse;
+
+while (true)
+{    
+    Console.Write("Введите номер дня недели от 1 до 7: ");
+
+    value = Console.ReadLine()?? "";
+    canParse = int.TryParse(value, out dayWeek);
+
+    if (canParse && (dayWeek >= 1 && dayWeek <= 7))
+    {
+            Console.WriteLine("{0} -> {1}", dayWeek, dayWeek > 5 ? "Да" : "Нет");
+            break;
+    }
+    else
+        Console.WriteLine("Невозможно сопоставить введенное число c днём недели");
+}
+
+
+
