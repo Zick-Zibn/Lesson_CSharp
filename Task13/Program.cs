86b50d30f2d1f8e851862e78244caed53768dde3
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-18 20:32:57 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿int     i;
+string  value;
+Boolean canParse;
+
+while (true)
+{
+    Console.Write("Введите число: ");
+
+    value = Console.ReadLine() ?? "";
+
+    canParse = int.TryParse(value, out i);
+
+    if (canParse && i > 99)
+    {
+        while (i > 999)
+            i /= 10;
+
+        i = i % 10;
+
+        Console.WriteLine($"{value} -> {i}");
+        break;
+    }
+    else
+        Console.WriteLine("Введенное число не является трехзначным");        
+}
