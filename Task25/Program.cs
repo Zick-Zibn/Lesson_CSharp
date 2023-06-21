663d5173dda15f048931d449f7023ca9107b7978
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-21 16:11:14 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,39 @@
+﻿int numberA = 0, numberB = 0;
+int result;
+
+GetInputNuber(ref numberA, ref numberB);
+
+Console.WriteLine($"{numberA},{numberB} -> {degreeNumber(numberA, numberB)}");
+
+static void GetInputNuber(ref int numberA, ref int numberB)
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите число А: ");
+            numberA = int.Parse(Console.ReadLine() ?? "");
+           
+            Console.Write("Введите число B: ");
+            numberB = int.Parse(Console.ReadLine() ?? "");
+
+            break;
+        }
+        catch(Exception exc)
+        {
+            Console.WriteLine("Ошибка ввода чисел {0}", exc.Message);
+        }
+    }
+}
+
+static int degreeNumber(int numberA, int numberB)
+{
+    int result = numberA;
+
+    for (int i = 1; i < numberB; i++)
+    {
+        result = result * numberA; 
+    }
+
+    return result;
+}
