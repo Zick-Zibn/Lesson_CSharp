19aa37f89ee773d3a85983465d5f86a4ce6a3678
Zick_Zibn <karpenko_ilia@list.ru>
2023-06-28 15:25:06 +0300

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Сколько чисел будет введено: ");
+int qtyNumber = int.Parse(Console.ReadLine()??"");
+int[] array  = InitArray(qtyNumber);
+
+Console.WriteLine($"{String.Join(",", array)} -> {CountPositiveNumber(array)}");
+
+int[] InitArray(int qtyNumber)
+{
+    int[] array = new int[qtyNumber];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        array[i] = int.Parse(Console.ReadLine()??"");
+    }
+    return array;
+}
+
+int CountPositiveNumber(int[] array)
+{
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            counter++;
+    }
+    return counter;
+}
