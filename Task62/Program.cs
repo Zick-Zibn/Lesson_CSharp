8e34a42007f7750d6f7a6ae6cc3ed35b363a550f
Zick_Zibn <karpenko_ilia@list.ru>
2023-07-08 15:11:32 +0300

    Task62

---
+++ Task62/Program.cs
@@ -0,0 +1,72 @@
+﻿int arraySize = 0;
+
+GetDataFromUser(ref arraySize);
+
+int[,] array = new int[arraySize, arraySize];
+
+FillArrayClockwise(array);
+PrintArray(array);
+
+void GetDataFromUser(ref int arraySize)
+{
+    bool inputWrong = false;
+
+    while (true)
+    {
+        try
+        {
+            inputWrong = false;
+
+            Console.Write("Введите число N для определения количества строк и столбцов квадратного массива: ");
+            arraySize = int.Parse(Console.ReadLine() ?? "");
+
+            if (arraySize <= 0)
+            {
+                inputWrong = true;
+                throw new Exception("Количество не может быть меньше или равно нулю");
+            }
+            break;
+        }
+        catch (Exception exc)
+        {
+            if (inputWrong)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            else
+                Console.WriteLine("Невозможно пребразовать введенное значение в число {0}", exc.Message);
+        }
+    }
+}
+
+void FillArrayClockwise(int[,] array)
+{
+    int row = 0, coll = 0, cnt = 0;;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        cnt++;
+        array[row, coll] = cnt;
+
+        if (row <= coll + 1 && row + coll < array.GetLength(1) - 1)
+            coll++;
+        else if (row < coll && row + coll >= array.GetLength(0) - 1)
+            row++;
+        else if (row >= coll && row + coll > array.GetLength(1) - 1)
+            coll--;
+        else
+            row--;
+    }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int k = 0; k < inArray.GetLength(1); k++)
+        {
+            Console.Write($"{inArray[i,k]}\t");   
+        }  
+        Console.WriteLine();
+    }
+}
