f96e3b773d0647c65d84db663336b7eefe950e21
Zick_Zibn <karpenko_ilia@list.ru>
2023-07-09 19:01:15 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine()??"");
+
+    Console.WriteLine($"{number} -> {CountNumbersToValue(number)}");
+
+    string CountNumbersToValue(int num)
+    {
+        if (num == 1)
+            return num.ToString();
+    
+        return num.ToString() + " " + CountNumbersToValue(num - 1);          
+    }
+}
+catch (Exception exc)
+{
+    Console.Write(exc.Message);
+}      
+
